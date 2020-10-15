7c8ef40b7905b6c14376316d22247f46fb3409e1
rian.digitalent <rian.digitalent@gmail.com>
2020-10-16 01:35:44 +0700

    debug init config

--- service-product/go.mod
+++ service-product/go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/riandigitalent/microservice1/utils v0.0.0-20201015171351-a186f62a2fa5
-    github.com/wskurniawan/intro-microservice/service-product v0.0.0-20201014173535-d19509851baa
+	github.com/spf13/viper v1.7.1
+	github.com/wskurniawan/intro-microservice/service-product v0.0.0-20201014173535-d19509851baa
 )
--- service-product/main.go
+++ service-product/main.go
@@ -6,14 +6,40 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/riandigitalent/microservice1/service-product/config"
 	"github.com/riandigitalent/microservice1/service-product/handler"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	cfg, err := getConfig()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
 	router := mux.NewRouter()
 
 	router.Handle("/add-product", http.HandlerFunc(handler.AddMenuHandler))
 
-	fmt.Println("Server listen on :8000")
-	log.Panic(http.ListenAndServe(":8000", router))
+	fmt.Printf("Server listen on :%s", cfg.Port)
+	log.Panic(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), router))
+}
+
+func getConfig() (config.Config, error) {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
 }
