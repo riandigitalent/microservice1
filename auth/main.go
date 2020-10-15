391f8bf450d54abe45075c706b28c341ad971179
rian.digitalent <rian.digitalent@gmail.com>
2020-10-16 02:37:21 +0700

    debug oauth

--- auth/main.go
+++ auth/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/riandigitalent/microservice/auth/handler"
+	"github.com/riandigitalent/microservice1/auth/handler"
 )
 
 func main() {
